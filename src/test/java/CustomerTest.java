2870615c83d919e528def7502566e28c48255f61
Yota <yoni.jmr@gmail.com>
2022-08-01 14:02:11 +0200

    Yohana-Customer-Class added last

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class CustomerTest {
+    private static Customer testCustomer;
+
+    public static void feelingConfident(){
+       Customer customer = new Customer();
+    }
+}
